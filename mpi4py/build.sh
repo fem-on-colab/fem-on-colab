c4688b58f5a40eb453622da620842ca898eeea54
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-05-26 09:49:38 +0200

    Add scripts for installation of openmpi 3.1 from PPA and mpi4py from github master

---
+++ mpi4py/build.sh
@@ -0,0 +1,19 @@
+# Copyright (C) 2021 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install gcc and MPI
+GCC_INSTALL_SCRIPT_PATH="gcc/install.sh" MPI4PY_ARCHIVE_PATH="skip" source mpi4py/install.sh
+
+# Install mpi4py
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user git+https://github.com/mpi4py/mpi4py.git
+
+# Prepare for installation
+tar czf mpi4py-install.tar.gz $INSTALL_PREFIX
+rm -rf $INSTALL_PREFIX
+mkdir -p $INSTALL_PREFIX
---
+++ mpi4py/install.sh
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install gcc
+GCC_INSTALL_SCRIPT_PATH=${GCC_INSTALL_SCRIPT_PATH:-"GCC_INSTALL_SCRIPT_PATH_IN"}
+[[ $GCC_INSTALL_SCRIPT_PATH == http* ]] && wget ${GCC_INSTALL_SCRIPT_PATH} -O /tmp/gcc-install.sh && GCC_INSTALL_SCRIPT_PATH=/tmp/gcc-install.sh
+source $GCC_INSTALL_SCRIPT_PATH
+
+# Install MPI
+add-apt-repository -y ppa:marmistrz/openmpi
+apt update
+apt install -y -qq libopenmpi-dev
+
+# Download and uncompress library archive
+MPI4PY_ARCHIVE_PATH=${MPI4PY_ARCHIVE_PATH:-"MPI4PY_ARCHIVE_PATH_IN"}
+[[ $MPI4PY_ARCHIVE_PATH == http* ]] && wget ${MPI4PY_ARCHIVE_PATH} -O /tmp/mpi4py-install.tar.gz && MPI4PY_ARCHIVE_PATH=/tmp/mpi4py-install.tar.gz
+[[ $MPI4PY_ARCHIVE_PATH != skip ]] && tar -xzf $MPI4PY_ARCHIVE_PATH --strip-components=2 --directory=/usr/local || true
