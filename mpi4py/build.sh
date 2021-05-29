2c2a07bc172b90bc4b790d901fb01733a85fabd2
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-05-29 14:36:06 +0200

    Compile openmpi from source, because it needs to enforce static linkage to libstdc++.so, and a libtool patch

--- mpi4py/build.sh
+++ mpi4py/build.sh
@@ -7,8 +7,23 @@
 set -e
 set -x
 
-# Install gcc and MPI
+# Install gcc
 GCC_INSTALL_SCRIPT_PATH="gcc/install.sh" MPI4PY_ARCHIVE_PATH="skip" source mpi4py/install.sh
+
+# Install MPI
+wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.gz -O /tmp/openmpi-src.tar.gz
+mkdir -p /tmp/openmpi-src
+tar -xvf /tmp/openmpi-src.tar.gz --directory /tmp/openmpi-src --strip-components 1
+cd /tmp/openmpi-src
+sed -i 's/AC_SUBST(CXXCPPFLAGS)/AC_SUBST(CXXCPPFLAGS)\npostdeps_CXX=`echo " $postdeps_CXX " | sed "s, -lstdc++ ,,g"`/g' configure.ac
+./autogen.pl --force
+./configure \
+    --build=x86_64-linux-gnu \
+    --prefix=$INSTALL_PREFIX \
+    --disable-silent-rules --disable-maintainer-mode --disable-dependency-tracking --disable-wrapper-runpath \
+    --enable-mpi-cxx
+make -j $(nproc) LDFLAGS="-Wc,-static-libgcc, -Wc,-static-libstdc++"
+make install LDFLAGS="-Wc,-static-libgcc, -Wc,-static-libstdc++"
 
 # Install mpi4py
 PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user git+https://github.com/mpi4py/mpi4py.git
--- mpi4py/install.sh
+++ mpi4py/install.sh
@@ -12,15 +12,6 @@ GCC_INSTALL_SCRIPT_PATH=${GCC_INSTALL_SCRIPT_PATH:-"https://fem-on-colab.github.
 [[ $GCC_INSTALL_SCRIPT_PATH == http* ]] && wget ${GCC_INSTALL_SCRIPT_PATH} -O /tmp/gcc-install.sh && GCC_INSTALL_SCRIPT_PATH=/tmp/gcc-install.sh
 source $GCC_INSTALL_SCRIPT_PATH
 
-# Install MPI
-add-apt-repository -y ppa:marmistrz/openmpi
-apt update
-apt install -y -qq libopenmpi-dev
-
-# Patch MPI to load libstdc++ from the newest gcc
-apt install -y -qq patchelf
-patchelf --set-rpath "/usr/local/lib:/usr/lib/x86_64-linux-gnu/openmpi/lib:/usr/lib/gcc/x86_64-linux-gnu/11:/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu" /usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi_cxx.so
-
 # Download and uncompress library archive
 MPI4PY_ARCHIVE_PATH=${MPI4PY_ARCHIVE_PATH:-"MPI4PY_ARCHIVE_PATH_IN"}
 [[ $MPI4PY_ARCHIVE_PATH == http* ]] && wget ${MPI4PY_ARCHIVE_PATH} -O /tmp/mpi4py-install.tar.gz && MPI4PY_ARCHIVE_PATH=/tmp/mpi4py-install.tar.gz
