21582be52c7cdcc180a43028f62a22f3e24bad6a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-05-30 11:43:05 +0200

    Add boost installation script

---
+++ boost/build.sh
@@ -0,0 +1,26 @@
+# Copyright (C) 2021 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install gcc
+GCC_INSTALL_SCRIPT_PATH="gcc/install.sh" BOOST_ARCHIVE_PATH="skip" source boost/install.sh
+
+# Install boost
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz -O /tmp/boost-src.tar.gz
+mkdir -p /tmp/boost-src
+tar -xvf /tmp/boost-src.tar.gz --directory /tmp/boost-src --strip-components 1
+cd /tmp/boost-src
+BOOST_TOOLSET="gcc"
+BOOST_CXXFLAGS="-std=c++17 $CPPFLAGS"
+BOOST_CXXFLAGS="cxxflags=${BOOST_CXXFLAGS// / cxxflags=}"
+BOOST_LDFLAGS="$LDFLAGS"
+BOOST_LDFLAGS="linkflags=${BOOST_LDFLAGS// / linkflags=}"
+export CPATH=$(python3 -c "from sysconfig import get_paths; print(get_paths()['include'])")
+./bootstrap.sh --with-toolset=$BOOST_TOOLSET --prefix="$INSTAL_PREFIX"
+./b2 toolset=$BOOST_TOOLSET $BOOST_CXXFLAGS $BOOST_LDFLAGS --prefix="$INSTAL_PREFIX" -j $(nproc) stage release
+./b2 install toolset=$BOOST_TOOLSET $BOOST_CXXFLAGS $BOOST_LDFLAGS --prefix="$INSTAL_PREFIX"
---
+++ boost/install.sh
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install gcc
+GCC_INSTALL_SCRIPT_PATH=${GCC_INSTALL_SCRIPT_PATH:-"https://fem-on-colab.github.io/releases/gcc-install.sh"}
+[[ $GCC_INSTALL_SCRIPT_PATH == http* ]] && wget ${GCC_INSTALL_SCRIPT_PATH} -O /tmp/gcc-install.sh && GCC_INSTALL_SCRIPT_PATH=/tmp/gcc-install.sh
+source $GCC_INSTALL_SCRIPT_PATH
+
+# Install zlib
+apt install -y -qq zlib1g-dev
+
+# Download and uncompress library archive
+BOOST_ARCHIVE_PATH=${BOOST_ARCHIVE_PATH:-"BOOST_ARCHIVE_PATH_IN"}
+[[ $BOOST_ARCHIVE_PATH == http* ]] && wget ${BOOST_ARCHIVE_PATH} -O /tmp/boost-install.tar.gz && BOOST_ARCHIVE_PATH=/tmp/boost-install.tar.gz
+if [[ $BOOST_ARCHIVE_PATH != skip ]]; then
+    tar -xzf $BOOST_ARCHIVE_PATH --strip-components=2 --directory=/usr/local
+fi
