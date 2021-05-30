d513bc6d38f3b94d653b4e33bda9a65810ec32c6
Francesco Ballarin <francesco.ballarin@unicatt.it>
2021-05-30 09:31:52 +0200

    Add slepc4py build script

---
+++ slepc4py/build.sh
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install petsc4py (and its dependencies)
+SLEPC4PY_ARCHIVE_PATH="skip" source slepc4py/install.sh
+
+# Install SLEPc
+git clone https://gitlab.com/slepc/slepc.git /tmp/slepc-src
+cd /tmp/slepc-src
+./configure --prefix=$INSTALL_PREFIX
+make SLEPC_DIR=$PWD PETSC_DIR=$INSTALL_PREFIX
+make SLEPC_DIR=$PWD PETSC_DIR=$INSTALL_PREFIX install
+rm -f $INSTALL_PREFIX/conf
+ln -s $INSTALL_PREFIX/lib/slepc/conf $INSTALL_PREFIX/conf
+
+# Install slepc4py
+cd /tmp/slepc-src/src/binding/slepc4py/
+PETSC_DIR=$INSTALL_PREFIX SLEPC_DIR=$INSTALL_PREFIX python3 setup.py install --prefix=$INSTALL_PREFIX
