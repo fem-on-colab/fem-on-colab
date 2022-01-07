2aa267879e3fc6d8d0947893236e9b2437f28a54
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-07 18:42:53 +0100

    Support multiple scalar types in petsc4py

--- petsc4py/build.sh
+++ petsc4py/build.sh
@@ -9,6 +9,13 @@ set -x
 
 REPODIR=$PWD
 
+# Expect one argument to set the scalar type
+: ${1?"Usage: $0 scalar_type"}
+SCALAR_TYPE="$1"
+if [[ "$SCALAR_TYPE" != "complex" ]]; then
+    SCALAR_TYPE="real"
+fi
+
 # Install h5py (and its dependencies, most notably gcc and mpi4py)
 PETSC4PY_ARCHIVE_PATH="skip" source petsc4py/install.sh
 
@@ -16,11 +23,7 @@ PETSC4PY_ARCHIVE_PATH="skip" source petsc4py/install.sh
 git clone https://gitlab.com/petsc/petsc.git /tmp/petsc-src
 cd /tmp/petsc-src
 patch -p1 < $REPODIR/petsc4py/patches/01-force-static-listdc++
-./configure \
-    --with-debugging=0 \
-    --with-hdf5-dir=$INSTALL_PREFIX \
-    --with-zlib-include=/usr/include \
-    --with-zlib-lib=/usr/lib/x86_64-linux-gnu/libz.so \
+DOWNLOADS="\
     --download-metis \
     --download-parmetis \
     --download-superlu \
@@ -28,9 +31,6 @@ patch -p1 < $REPODIR/petsc4py/patches/01-force-static-listdc++
     --download-blacs \
     --download-scalapack \
     --download-mumps \
-    --download-hypre \
-    --download-spai \
-    --download-ml \
     --download-hwloc \
     --download-ptscotch \
     --download-suitesparse \
@@ -41,6 +41,21 @@ patch -p1 < $REPODIR/petsc4py/patches/01-force-static-listdc++
     --download-netcdf \
     --download-pnetcdf \
     --download-eigen \
+"
+if [[ "$SCALAR_TYPE" != "complex" ]]; then
+    DOWNLOADS="$DOWNLOADS \
+        --download-hypre \
+        --download-spai \
+        --download-ml \
+    "
+fi
+./configure \
+    --with-scalar-type=$SCALAR_TYPE \
+    --with-debugging=0 \
+    --with-hdf5-dir=$INSTALL_PREFIX \
+    --with-zlib-include=/usr/include \
+    --with-zlib-lib=/usr/lib/x86_64-linux-gnu/libz.so \
+    $DOWNLOADS \
     --prefix=$INSTALL_PREFIX \
     CPPFLAGS="-fPIC $CPPFLAGS" \
     COPTFLAGS="-g -O3 $CPPFLAGS" \
