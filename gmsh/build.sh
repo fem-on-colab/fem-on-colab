c598dda04748c2919589c24a6d58e5b1fa02835d
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-10 17:34:58 +0100

    Use packaged OCC in gmsh build script

--- gmsh/build.sh
+++ gmsh/build.sh
@@ -7,31 +7,8 @@
 set -e
 set -x
 
-# Install h5py (and its dependencies, most notably gcc and mpi4py)
+# Install h5py and OCC
 GMSH_ARCHIVE_PATH="skip" source gmsh/install.sh
-
-# Install OCC dependencies (already available on Colab)
-apt install -y -qq libfontconfig1-dev libfreetype6-dev libx11-dev libxi-dev libxmu-dev libgl1-mesa-dev mesa-common-dev
-
-# Install OCC
-wget "http://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_6_0;sf=tgz" -O /tmp/occt-src.tgz
-mkdir -p /tmp/occt-src
-tar -xzvf /tmp/occt-src.tgz --directory /tmp/occt-src --strip-components 1
-cd /tmp/occt-src
-sed -i "s/pthread rt stdc++/pthread rt/g" adm/cmake/occt_csf.cmake
-mkdir -p /tmp/occt-src/build
-cd /tmp/occt-src/build
-cmake \
-    -DCMAKE_CXX_FLAGS="$CPPFLAGS" \
-    -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" \
-    -DCMAKE_SKIP_RPATH:BOOL=ON \
-    -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_MODULE_ApplicationFramework=0 \
-    -DBUILD_MODULE_Draw=0 \
-    -DBUILD_MODULE_Visualization=0 \
-    ..
-make -j $(nproc) install
 
 # Install gmsh
 git clone https://gitlab.onelab.info/gmsh/gmsh.git /tmp/gmsh-src
--- gmsh/install.sh
+++ gmsh/install.sh
@@ -12,6 +12,11 @@ H5PY_INSTALL_SCRIPT_PATH=${H5PY_INSTALL_SCRIPT_PATH:-"H5PY_INSTALL_SCRIPT_PATH_I
 [[ $H5PY_INSTALL_SCRIPT_PATH == http* ]] && wget ${H5PY_INSTALL_SCRIPT_PATH} -O /tmp/h5py-install.sh && H5PY_INSTALL_SCRIPT_PATH=/tmp/h5py-install.sh
 source $H5PY_INSTALL_SCRIPT_PATH
 
+# Install OCC (and its dependencies, most notably gcc)
+OCC_INSTALL_SCRIPT_PATH=${OCC_INSTALL_SCRIPT_PATH:-"OCC_INSTALL_SCRIPT_PATH_IN"}
+[[ $OCC_INSTALL_SCRIPT_PATH == http* ]] && wget ${OCC_INSTALL_SCRIPT_PATH} -O /tmp/occ-install.sh && OCC_INSTALL_SCRIPT_PATH=/tmp/occ-install.sh
+source $OCC_INSTALL_SCRIPT_PATH
+
 # Download and uncompress library archive
 GMSH_ARCHIVE_PATH=${GMSH_ARCHIVE_PATH:-"GMSH_ARCHIVE_PATH_IN"}
 [[ $GMSH_ARCHIVE_PATH == http* ]] && wget ${GMSH_ARCHIVE_PATH} -O /tmp/gmsh-install.tar.gz && GMSH_ARCHIVE_PATH=/tmp/gmsh-install.tar.gz
@@ -22,7 +27,6 @@ fi
 # Add symbolic links to gmsh libraries in /usr/lib, because Colab does not export /usr/local/lib to LD_LIBRARY_PATH
 if [[ $GMSH_ARCHIVE_PATH != skip ]]; then
     ln -fs /usr/local/lib/libgmsh*.so* /usr/lib
-    ln -fs /usr/local/lib/libTK*.so* /usr/lib
 fi
 
 # Install X11 for gmsh
