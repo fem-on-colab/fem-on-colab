53ac960d78e05e24f558bb7b7ba39cd7d384c691
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-16 13:47:11 +0100

    Add itk package

---
+++ itk/build.sh
@@ -0,0 +1,19 @@
+# Copyright (C) 2021-2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install vtk
+ITK_ARCHIVE_PATH="skip" source itk/install.sh
+
+# Install patchelf
+apt install -y -qq patchelf
+
+# Install itk from wheels and patch it
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user itk
+find $INSTALL_PREFIX -name "*\.so" -exec patchelf --remove-needed libstdc++.so.6 {} \;
+find $INSTALL_PREFIX -name "*\.so.*" -exec patchelf --remove-needed libstdc++.so.6 {} \;
