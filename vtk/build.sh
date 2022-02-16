8d8a3e216c90aefc20005112613e465c4bb67da7
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-16 12:32:27 +0100

    Add vtk package

---
+++ vtk/build.sh
@@ -0,0 +1,30 @@
+# Copyright (C) 2021-2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install gcc
+VTK_ARCHIVE_PATH="skip" source vtk/install.sh
+
+# Install patchelf from source, as the version packaged by ubuntu:18.04 is too old
+git clone https://github.com/NixOS/patchelf.git /tmp/patchelf-src
+cd /tmp/patchelf-src
+TAGS=($(git tag -l --sort=-version:refname))
+echo "Latest tag is ${TAGS[0]}"
+git checkout ${TAGS[0]}
+./bootstrap.sh
+./configure --prefix=$INSTALL_PREFIX
+make
+make install
+
+# Install vtk from wheels and patch it
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user vtk
+find $INSTALL_PREFIX -name "*\.so" -exec patchelf --replace-needed libstdc++.so.6 /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so {} \;
+find $INSTALL_PREFIX -name "*\.so.*" -exec patchelf --replace-needed libstdc++.so.6 /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so {} \;
+
+# Install xvfbwrapper too
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user xvfbwrapper
---
+++ vtk/install.sh
@@ -0,0 +1,33 @@
+# Copyright (C) 2021-2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Check for existing installation
+SHARE_PREFIX="/usr/local/share/fem-on-colab"
+VTK_INSTALLED="$SHARE_PREFIX/vtk.installed"
+
+if [[ ! -f $VTK_INSTALLED ]]; then
+    # Install gcc
+    GCC_INSTALL_SCRIPT_PATH=${GCC_INSTALL_SCRIPT_PATH:-"GCC_INSTALL_SCRIPT_PATH_IN"}
+    [[ $GCC_INSTALL_SCRIPT_PATH == http* ]] && GCC_INSTALL_SCRIPT_DOWNLOAD=${GCC_INSTALL_SCRIPT_PATH} && GCC_INSTALL_SCRIPT_PATH=/tmp/gcc-install.sh && [[ ! -f ${GCC_INSTALL_SCRIPT_PATH} ]] && wget ${GCC_INSTALL_SCRIPT_DOWNLOAD} -O ${GCC_INSTALL_SCRIPT_PATH}
+    source $GCC_INSTALL_SCRIPT_PATH
+
+    # Download and uncompress library archive
+    VTK_ARCHIVE_PATH=${VTK_ARCHIVE_PATH:-"VTK_ARCHIVE_PATH_IN"}
+    [[ $VTK_ARCHIVE_PATH == http* ]] && VTK_ARCHIVE_DOWNLOAD=${VTK_ARCHIVE_PATH} && VTK_ARCHIVE_PATH=/tmp/vtk-install.tar.gz && wget ${VTK_ARCHIVE_DOWNLOAD} -O ${VTK_ARCHIVE_PATH}
+    if [[ $VTK_ARCHIVE_PATH != skip ]]; then
+        tar -xzf $VTK_ARCHIVE_PATH --strip-components=2 --directory=/usr/local
+    fi
+
+    # Install X11
+    apt install -y -qq libgl1-mesa-dev libxrender1 xvfb
+
+    # Mark package as installed
+    mkdir -p $SHARE_PREFIX
+    touch $VTK_INSTALLED
+fi
