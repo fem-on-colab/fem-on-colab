1477b6acadd7bed936d9c577a0337760ce968982
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-09-02 08:08:14 +0200

    Install itkwidgets alongside itk

--- itk/README.md
+++ itk/README.md
@@ -1,1 +1,3 @@
-[![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/fem-on-colab/fem-on-colab/blob/main/itk/test.ipynb)
+itk: [![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/fem-on-colab/fem-on-colab/blob/main/itk/test-itk.ipynb)
+
+itkwidgets: [![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/fem-on-colab/fem-on-colab/blob/main/itk/test-itkwidgets.ipynb)
--- itk/build.sh
+++ itk/build.sh
@@ -14,3 +14,16 @@ ITK_ARCHIVE_PATH="skip" source itk/install.sh
 PYTHONUSERBASE=$INSTALL_PREFIX pip3 install --user --pre itk
 find $INSTALL_PREFIX -name "*\.so" -exec patchelf --replace-needed libstdc++.so.6 /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so {} \;
 find $INSTALL_PREFIX -name "*\.so.*" -exec patchelf --replace-needed libstdc++.so.6 /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so {} \;
+
+# Install zstandard
+git clone https://github.com/indygreg/python-zstandard.git /tmp/zstandard-src
+cd /tmp/zstandard-src/
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install . --user
+
+# Install itkwidgets (legacy branch)
+git clone https://github.com/InsightSoftwareConsortium/itkwidgets.git /tmp/itkwidgets-src
+cd /tmp/itkwidgets-src
+git checkout master
+patch -p 1 < $REPODIR/itk/patches/01-unpin-itk-in-itkwidgets
+patch -p 1 < $REPODIR/itk/patches/02-enable-custom-widget-manager-in-itkwidgets
+PYTHONUSERBASE=$INSTALL_PREFIX pip3 install . --user
