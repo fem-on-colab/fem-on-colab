7cdd20de6708d7fe9f15d4387322bfc1e351a112
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-09-08 08:38:31 +0200

    Build gcc 12 from source

---
+++ gcc/build.sh
@@ -0,0 +1,67 @@
+# Copyright (C) 2021-2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -x
+
+# Install zlib
+GCC_ARCHIVE_PATH="skip" source gcc/install.sh
+
+# Compile gcc
+GCC_VERSION="12"
+GCC_REPO="https://gcc.gnu.org/git/gcc.git"
+GCC_TAG=$(git -c "versionsort.suffix=-" ls-remote --tags --sort=-version:refname ${GCC_REPO} "releases/gcc-${GCC_VERSION}.[0-9].[0-9]" | head --lines=1 | cut --delimiter='/' --fields=3-)
+echo "Latest tag in the v${GCC_VERSION} series is ${GCC_TAG}"
+git clone --depth 1 --branch ${GCC_TAG} ${GCC_REPO} /tmp/gcc-src
+cd /tmp/gcc-src
+./contrib/download_prerequisites
+sed -i 's|    \\\$(top_builddir)/../libstdc++-v3/src/libstdc++.la"|    "|g' config/libstdc++-raw-cxx.m4
+sed -i 's|AC_DEFUN(\[_GCC_AUTOCONF_VERSION_CHECK\]|AC_DEFUN([_GCC_AUTOCONF_VERSION_CHECK_DISABLED]|g' config/override.m4
+sed -i 's|_GCC_AUTOCONF_VERSION_CHECK||g' config/override.m4
+sed -i 's|gcc/xg++ -B$$r/$(HOST_SUBDIR)/gcc/ -nostdinc++|gcc/xg++ -B$$r/$(HOST_SUBDIR)/gcc/ -nostdinc++ -static-libstdc++|g' configure.ac
+sed -i 's|gcc/xgcc -shared-libgcc -B$$r/$(HOST_SUBDIR)/gcc -nostdinc++|gcc/xg++ -static-libgcc -B$$r/$(HOST_SUBDIR)/gcc -nostdinc++ -static-libstdc++|g' configure.ac
+find ./ -type f -name 'configure.ac' -exec sed -i 's@AC_OUTPUT@postdeps_CXX=`echo " \$postdeps_CXX " | sed "s, \-lstdc++ ,,g"`\nAC_OUTPUT@g' {} \;
+find ./ -name configure | while read f; do d=$( dirname "$f" ) && echo "$d": && l=$d/regenerate.log && ( cd "$d"/ && if test -f Makefile.am; then autoreconf; else autoconf; fi ) > "$l" 2>&1; if test -s "$l"; then echo "Review '$l'"; else rm "$l"; fi; done
+./configure \
+    --prefix=$INSTALL_PREFIX \
+    \
+    --build=x86_64-linux-gnu \
+    --host=x86_64-linux-gnu \
+    --target=x86_64-linux-gnu \
+    \
+    --disable-bootstrap \
+    --disable-multilib \
+    --disable-vtable-verify \
+    --enable-checking=release \
+    --enable-clocale=gnu \
+    --enable-default-pie \
+    --enable-gnu-unique-object \
+    --enable-languages=c,c++,fortran \
+    --enable-libphobos-checking=release \
+    --enable-libstdcxx-debug \
+    --enable-libstdcxx-time=yes \
+    --enable-linker-build-id \
+    --enable-nls \
+    --enable-plugin \
+    --enable-shared \
+    --enable-threads=posix \
+    --program-suffix=-${GCC_VERSION} \
+    --with-default-libstdcxx-abi=new \
+    --with-gcc-major-version-only \
+    --with-system-zlib \
+    --with-target-system-zlib=auto \
+    --with-tune=generic \
+    --without-included-gettext
+make -j $(nproc)
+
+# Install
+mkdir -p ${INSTALL_PREFIX}/lib
+ln -s ${INSTALL_PREFIX}/lib ${INSTALL_PREFIX}/lib64
+make install
+
+# Add symbolic links to programs without version suffix
+cd ${INSTALL_PREFIX}/bin
+find * -name "*-${GCC_VERSION}" -exec bash -c 'ln -s "$1" "${1/-$2/}"' -- {} ${GCC_VERSION} \;
--- gcc/install.sh
+++ gcc/install.sh
@@ -12,47 +12,55 @@ SHARE_PREFIX="/usr/local/share/fem-on-colab"
 GCC_INSTALLED="$SHARE_PREFIX/gcc.installed"
 
 if [[ ! -f $GCC_INSTALLED ]]; then
-    # Install gcc 11
-    add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    apt update
-    apt install -y -qq gcc-11 gfortran-11 g++-11 libgcc1
-    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7
-    update-alternatives --install /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-7 7
-    update-alternatives --install /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-7 7
-    update-alternatives --install /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-7 7
-    # update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-7 7 # was never installed
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-g++ x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-7 7
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-7 7
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ar x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-7 7
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-nm x86_64-linux-gnu-gcc-nm /usr/bin/x86_64-linux-gnu-gcc-nm-7 7
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ranlib x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-7 7
-    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 11
-    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 11
-    update-alternatives --install /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-11 11
-    update-alternatives --install /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-11 11
-    update-alternatives --install /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-11 11
-    update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-11 11
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-g++ x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-11 11
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-11 11
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ar x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-11 11
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-nm x86_64-linux-gnu-gcc-nm /usr/bin/x86_64-linux-gnu-gcc-nm-11 11
-    update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ranlib x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-11 11
-    update-alternatives --set g++ /usr/bin/g++-11
-    update-alternatives --set gcc /usr/bin/gcc-11
-    update-alternatives --set gcc-ar /usr/bin/gcc-ar-11
-    update-alternatives --set gcc-nm /usr/bin/gcc-nm-11
-    update-alternatives --set gcc-ranlib /usr/bin/gcc-ranlib-11
-    update-alternatives --set gfortran /usr/bin/gfortran-11
-    update-alternatives --set x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-11
-    update-alternatives --set x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-11
-    update-alternatives --set x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-11
-    update-alternatives --set x86_64-linux-gnu-gcc-nm /usr/bin/x86_64-linux-gnu-gcc-nm-11
-    update-alternatives --set x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-11
+    INSTALL_PREFIX="/usr/local"
 
-    # Copy libstdc++ from the newest gcc due to possible rpath patching
-    cp -f /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so /usr/lib/gcc/x86_64-linux-gnu/11/libstdc++.so.6
+    # Download and uncompress library archive
+    GCC_ARCHIVE_PATH=${GCC_ARCHIVE_PATH:-"GCC_ARCHIVE_PATH_IN"}
+    [[ $GCC_ARCHIVE_PATH == http* ]] && GCC_ARCHIVE_DOWNLOAD=${GCC_ARCHIVE_PATH} && GCC_ARCHIVE_PATH=/tmp/gcc-install.tar.gz && wget ${GCC_ARCHIVE_DOWNLOAD} -O ${GCC_ARCHIVE_PATH}
+    if [[ $GCC_ARCHIVE_PATH != skip ]]; then
+        tar -xzf $GCC_ARCHIVE_PATH --strip-components=2 --directory=${INSTALL_PREFIX}
+    fi
 
+    # Install zlib
+    apt install -y -qq zlib1g-dev
+
+    # Set alternatives
+    if [[ $GCC_ARCHIVE_PATH != skip ]]; then
+        GCC_VERSION="12"
+        update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 7
+        update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 7
+        update-alternatives --install /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-7 7
+        update-alternatives --install /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-7 7
+        update-alternatives --install /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-7 7
+        # update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-7 7 # was never installed
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-g++ x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-7 7
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-7 7
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ar x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-7 7
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-nm x86_64-linux-gnu-gcc-nm /usr/bin/x86_64-linux-gnu-gcc-nm-7 7
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ranlib x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-7 7
+        update-alternatives --install /usr/bin/g++ g++ ${INSTALL_PREFIX}/bin/g++-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/gcc gcc ${INSTALL_PREFIX}/bin/gcc-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/gcc-ar gcc-ar ${INSTALL_PREFIX}/bin/gcc-ar-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/gcc-nm gcc-nm ${INSTALL_PREFIX}/bin/gcc-nm-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/gcc-ranlib gcc-ranlib ${INSTALL_PREFIX}/bin/gcc-ranlib-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/gfortran gfortran ${INSTALL_PREFIX}/bin/gfortran-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-g++ x86_64-linux-gnu-g++ ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-g++-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc x86_64-linux-gnu-gcc ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ar x86_64-linux-gnu-gcc-ar ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-ar-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-nm x86_64-linux-gnu-gcc-nm ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-nm-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --install /usr/bin/x86_64-linux-gnu-gcc-ranlib x86_64-linux-gnu-gcc-ranlib ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-ranlib-${GCC_VERSION} ${GCC_VERSION}
+        update-alternatives --set g++ ${INSTALL_PREFIX}/bin/g++-${GCC_VERSION}
+        update-alternatives --set gcc ${INSTALL_PREFIX}/bin/gcc-${GCC_VERSION}
+        update-alternatives --set gcc-ar ${INSTALL_PREFIX}/bin/gcc-ar-${GCC_VERSION}
+        update-alternatives --set gcc-nm ${INSTALL_PREFIX}/bin/gcc-nm-${GCC_VERSION}
+        update-alternatives --set gcc-ranlib ${INSTALL_PREFIX}/bin/gcc-ranlib-${GCC_VERSION}
+        update-alternatives --set gfortran ${INSTALL_PREFIX}/bin/gfortran-${GCC_VERSION}
+        update-alternatives --set x86_64-linux-gnu-g++ ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-g++-${GCC_VERSION}
+        update-alternatives --set x86_64-linux-gnu-gcc ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-${GCC_VERSION}
+        update-alternatives --set x86_64-linux-gnu-gcc-ar ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-ar-${GCC_VERSION}
+        update-alternatives --set x86_64-linux-gnu-gcc-nm ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-nm-${GCC_VERSION}
+        update-alternatives --set x86_64-linux-gnu-gcc-ranlib ${INSTALL_PREFIX}/bin/x86_64-linux-gnu-gcc-ranlib-${GCC_VERSION}
+    fi
 
     # Mark package as installed
     mkdir -p $SHARE_PREFIX
