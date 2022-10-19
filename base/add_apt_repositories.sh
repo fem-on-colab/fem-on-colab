569706ee0a5d5baa3cfadc72e9c3ff74ba5224d0
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-10-19 12:14:31 +0200

    Remove a few unrequired packages to limit docker image size

--- base/add_apt_repositories.sh
+++ base/add_apt_repositories.sh
@@ -11,19 +11,6 @@ apt install -y -qq software-properties-common wget
 # Python (as on Colab)
 add-apt-repository -y ppa:deadsnakes/ppa
 
-# R (as on Colab)
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9
-apt-add-repository -y 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
-add-apt-repository -y ppa:cran/libgit2
-add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
-
-# CUDA (as on Colab)
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F60F4B3D7FA2AF80
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-apt-add-repository -y 'deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/ /'
-apt-add-repository -y 'deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /'
-add-apt-repository -y ppa:graphics-drivers/ppa
-
 # CMake (actually newer than the one on Colab)
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ bionic main'
--- base/install_apt_packages.sh
+++ base/install_apt_packages.sh
@@ -4,8 +4,23 @@
 #
 # SPDX-License-Identifier: MIT
 
-# Install same packages as on Colab
-APT_PACKAGES=$(grep -v -e "^#" -e "^Listing..." -h ${COLAB_BACKEND_INFO}/apt-list.txt | cut -f1 -d / | tr "\n" " ")
+# Get packages list from Colab backend info
+grep -v -e "^#" -e "^Listing..." -h ${COLAB_BACKEND_INFO}/apt-list.txt > ${COLAB_BACKEND_INFO}/apt-list-clean.txt
+
+# Remove cuda packages to decrease the image size
+grep -v -e "^cuda-" -e "^libcu" -e "^libopencv" -e "^libncc" -e "^libnpp" -e "^libnvidia" -e "^libnvjpeg" -e "^libvdpau" -e "^libxnvctrl" -e "^nsight-compute" -e "^nsight-systems" -e "^nvidia" -e "^ocl" -e "^opencl" -e "^vdpau-driver" -e "^xserver-xorg-video-nvidia" -h ${COLAB_BACKEND_INFO}/apt-list-clean.txt > ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt
+mv ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt ${COLAB_BACKEND_INFO}/apt-list-clean.txt
+
+# Remove R packages to decrease the image size
+grep -v -e "^r-" -e "^libgit2" -e "^libmbedcrypto" -e "^libmbedtls" -e "^libmbedx" -h ${COLAB_BACKEND_INFO}/apt-list-clean.txt > ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt
+mv ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt ${COLAB_BACKEND_INFO}/apt-list-clean.txt
+
+# Remove Java packages to decrease the image size
+grep -v -e "^ca-certificates-java" -e "^default-jre" -e "^java-common" -e "^libprotobuf-java" -e "^openjdk-" -h ${COLAB_BACKEND_INFO}/apt-list-clean.txt > ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt
+mv ${COLAB_BACKEND_INFO}/apt-list-clean-tmp.txt ${COLAB_BACKEND_INFO}/apt-list-clean.txt
+
+# Install the remaining packages from Colab backend info
+APT_PACKAGES=$(cat ${COLAB_BACKEND_INFO}/apt-list-clean.txt | cut -f1 -d / | tr "\n" " ")
 apt install -y -qq ${APT_PACKAGES}
 
 # Install additional packages that are required to compile from source
