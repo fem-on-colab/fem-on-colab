9f4c6749d4c854a29d6412c25446b53ed5de278d
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-10-27 08:51:36 +0200

    Disable ICU support while building boost, as it would introduce a dependency on system libstdc++.so

--- boost/build.sh
+++ boost/build.sh
@@ -23,6 +23,6 @@ BOOST_LDFLAGS="$LDFLAGS"
 BOOST_LDFLAGS="linkflags=${BOOST_LDFLAGS// / linkflags=}"
 BOOST_PYTHON_EXEC="$(which python3)"
 export CPATH=$(python3 -c "from sysconfig import get_paths; print(get_paths()['include'])")
-./bootstrap.sh --with-toolset=$BOOST_TOOLSET --with-python=$BOOST_PYTHON_EXEC --prefix="$INSTAL_PREFIX"
-./b2 toolset=$BOOST_TOOLSET $BOOST_CXXFLAGS $BOOST_LDFLAGS --prefix="$INSTAL_PREFIX" -j $(nproc) stage release
+./bootstrap.sh --with-toolset=$BOOST_TOOLSET --with-python=$BOOST_PYTHON_EXEC --without-icu --prefix="$INSTAL_PREFIX"
+./b2 toolset=$BOOST_TOOLSET $BOOST_CXXFLAGS $BOOST_LDFLAGS --disable-icu --prefix="$INSTAL_PREFIX" -j $(nproc) stage release
 ./b2 install toolset=$BOOST_TOOLSET $BOOST_CXXFLAGS $BOOST_LDFLAGS --prefix="$INSTAL_PREFIX"
