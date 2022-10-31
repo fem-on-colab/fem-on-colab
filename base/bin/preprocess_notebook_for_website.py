75ebef2a71fa60459bc751c5b358a6484ca13e72
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-10-31 18:54:34 +0100

    Add optional preprocessing of notebooks to be posted on the website

---
+++ base/bin/preprocess_notebook_for_website.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2021-2022 by the FEM on Colab authors
+#
+# This file is part of FEM on Colab.
+#
+# SPDX-License-Identifier: MIT
+"""Preprocess notebook for website."""
+
+import sys
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 3
