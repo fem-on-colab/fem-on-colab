60c3555de1757516d746f1cd04296cef68b3064f
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-12-01 11:40:19 +0100

    Bump python version to 3.8

--- base/setup_python.sh
+++ base/setup_python.sh
@@ -9,8 +9,8 @@ set -x
 
 # Setup non-default python version
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-update-alternatives --set python3 /usr/bin/python3.7
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+update-alternatives --set python3 /usr/bin/python3.8
 
 # Changing system-wide python version may break apt
 sed -i 's|#!/usr/bin/python3|#!/usr/bin/python3.6|' /usr/bin/add-apt-repository
@@ -19,6 +19,6 @@ sed -i 's|#!/usr/bin/python3|#!/usr/bin/python3.6|' /usr/bin/add-apt-repository
 PIP_RELEASE_URL=$(curl -s https://pypi.org/pypi/pip/json | jq -r ".urls[0].url")
 PIP_RELEASE_FILE=${PIP_RELEASE_URL##*/}
 wget ${PIP_RELEASE_URL} -O ${PIP_RELEASE_FILE}
-export PYTHONPATH=/usr/lib/python3.7/dist-packages:/usr/lib/python3.7/site-packages
+export PYTHONPATH=/usr/lib/python3.8/dist-packages:/usr/lib/python3.8/site-packages
 PYTHONUSERBASE=/usr python3 ${PIP_RELEASE_FILE}/pip install --no-index --user ${PIP_RELEASE_FILE}
 rm -f ${PIP_RELEASE_FILE}
