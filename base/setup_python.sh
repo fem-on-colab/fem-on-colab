7f7ca1779459e2dbc9a2c1ce6be269aac456acb4
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-01-13 20:36:23 -0500

    deadsnakes ppa is not required anymore since ubuntu 20.04 already ships python3.8

--- base/setup_python.sh
+++ base/setup_python.sh
@@ -7,14 +7,6 @@
 set -e
 set -x
 
-# Setup non-default python version
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-update-alternatives --set python3 /usr/bin/python3.8
-
-# Changing system-wide python version may break apt
-sed -i 's|#!/usr/bin/python3|#!/usr/bin/python3.6|' /usr/bin/add-apt-repository
-
 # Download latest pip release
 PIP_RELEASE_URL=$(curl -s https://pypi.org/pypi/pip/json | jq -r ".urls[0].url")
 PIP_RELEASE_FILE=${PIP_RELEASE_URL##*/}
