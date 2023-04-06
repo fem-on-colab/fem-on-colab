5f871c3082b4b89128a7d876b56f41eebda38fb3
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-04-06 08:34:06 +0200

    Pyvista will soon drop support of ipygany and panel. Since trame doesn't work yet on Colab, we will continue using panel by calling directly the panel library itself, rather than through pyvista

--- vtk/build.sh
+++ vtk/build.sh
@@ -71,10 +71,7 @@ cd /tmp/pyvista-src
 TAGS=($(git tag -l --sort=-version:refname "v[0-9].[0-9]*.[0-9]"))
 echo "Latest tag is ${TAGS[0]}"
 git checkout ${TAGS[0]}
-patch -p 1 < $REPODIR/vtk/patches/01-ipygany
-patch -p 1 < $REPODIR/vtk/patches/02-panel
-patch -p 1 < $REPODIR/vtk/patches/03-default-backend
-patch -p 1 < $REPODIR/vtk/patches/04-disable-xvfb-warning
+patch -p 1 < $REPODIR/vtk/patches/01-start-xvfb
 PYTHONUSERBASE=$INSTALL_PREFIX python3 -m pip install . --user
 cd -
 
