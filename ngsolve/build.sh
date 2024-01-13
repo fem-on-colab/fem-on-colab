a9d9c1c8df17dee35bbe7e28ff26dbcca3c5f0f5
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-13 07:53:13 +0100

    Clarify comment in ngsolve build script

--- ngsolve/build.sh
+++ ngsolve/build.sh
@@ -20,7 +20,7 @@ fi
 # Install OCC dependencies
 apt install -y -qq libfontconfig1-dev libfreetype6-dev libx11-dev libxi-dev libxmu-dev libgl1-mesa-dev mesa-common-dev
 
-# Install OCC, petsc4py and pybind11 (and their dependencies)
+# Install OCC, pybind11 and slepc4py (and their dependencies)
 NGSOLVE_ARCHIVE_PATH="skip" source ngsolve/install.sh
 
 # netgen
