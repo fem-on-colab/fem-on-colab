2becd0a506b2e9b19e3f6fcc42b0cd020c31836a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-22 07:16:59 +0100

    Update hdf5 to use the 1.14 series

--- h5py/build.sh
+++ h5py/build.sh
@@ -13,11 +13,11 @@ H5PY_ARCHIVE_PATH="skip" source h5py/install.sh
 # Install HDF5
 git clone https://github.com/HDFGroup/hdf5.git /tmp/hdf5-src
 cd /tmp/hdf5-src
-TAGS=($(git tag -l --sort=-version:refname "hdf5-1_10_[0-9][0-9]"))
+TAGS=($(git tag -l --sort=-version:refname "hdf5-1_14_[0-9][0-9]"))
 if [ ${#TAGS[@]} -eq 0 ]; then
-    TAGS=($(git tag -l --sort=-version:refname "hdf5-1_10_[0-9]"))
+    TAGS=($(git tag -l --sort=-version:refname "hdf5-1_14_[0-9]"))
 fi
-echo "Latest tag in the v1.10 series is ${TAGS[0]}"
+echo "Latest tag in the v1.14 series is ${TAGS[0]}"
 git checkout ${TAGS[0]}
 ./configure \
     --enable-parallel \
