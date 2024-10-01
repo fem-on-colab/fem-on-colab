ab7e93a1f8ba0f639c65f3cd618fa168fd57d099
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-10-01 14:16:32 +0200

    Revert "Temporarily patch Boost::Python until numpy 2.0 is supported in a Boost release"
    This reverts commit 5bf472a230c3956e687e5e0a740c5b1be8000db8.

--- boost/build.sh
+++ boost/build.sh
@@ -16,12 +16,6 @@ cd /tmp/boost-src
 TAGS=($(git tag -l --sort=-version:refname "boost-1.[0-9][0-9].[0-9]"))
 echo "Latest tag in the v1 series is ${TAGS[0]}"
 git checkout ${TAGS[0]}
-cd libs/python
-wget https://github.com/boostorg/python/commit/0474de0f6cc9c6e7230aeb7164af2f7e4ccf74bf.diff -O PR432.diff
-patch -p1 < PR432.diff
-wget https://github.com/boostorg/python/commit/99a5352b5cf790c559a7b976c1ba99520431d9d1.diff -O PR443.diff
-patch -p1 < PR443.diff
-cd ../..
 BOOST_TOOLSET="gcc"
 BOOST_CXXFLAGS="cxxflags=-std=c++17"
 BOOST_PYTHON_EXEC="$(which python3)"
