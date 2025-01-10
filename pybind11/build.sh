3b71f2798bed01671d15c13e88c024fa68399769
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-01-10 15:53:31 +0100

    Use latest tagged version for pybind11 and nanobind

--- pybind11/build.sh
+++ pybind11/build.sh
@@ -14,6 +14,9 @@ PYBIND11_ARCHIVE_PATH="skip" source pybind11/install.sh
 # Install pybind11
 git clone https://github.com/pybind/pybind11.git /tmp/pybind11-src
 cd /tmp/pybind11-src
+TAGS=($(git tag -l --sort=-version:refname "v[0-9].[0-9].[0-9]"))
+echo "Latest tag is ${TAGS[0]}"
+git checkout ${TAGS[0]}
 cmake \
     -D CMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX \
     -D PYBIND11_TEST=off \
@@ -26,6 +29,9 @@ cd && rm -rf /tmp/pybind11-src
 # Install nanobind
 git clone https://github.com/wjakob/nanobind.git /tmp/nanobind-src
 cd /tmp/nanobind-src
+TAGS=($(git tag -l --sort=-version:refname "v[0-9].[0-9].[0-9]"))
+echo "Latest tag is ${TAGS[0]}"
+git checkout ${TAGS[0]}
 git submodule update --init --recursive
 cmake \
     -D CMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX \
