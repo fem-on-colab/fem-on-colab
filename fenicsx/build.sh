dbceae9f885bcaff159d55ffffd847dd67872035
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-08-15 21:13:59 +0200

    Provide a shim std::format for dolfinx until Colab upgrades from gcc 12 to gcc 13

--- fenicsx/build.sh
+++ fenicsx/build.sh
@@ -96,6 +96,69 @@ cmake \
 make -j $(nproc) install
 cd && rm -rf /tmp/pugixml-src
 
+# fmt, required for building DOLFINx until gcc gets upgraded to 13.0
+mkdir -p /tmp/format-check
+cd /tmp/format-check
+cat << EOF > format-check.cpp
+#include <format>
+#include <string>
+int main() { auto s = std::format("{}", 1); }
+EOF
+if ! g++ -std=c++20 format-check.cpp -o format-check >/dev/null 2>&1; then
+    git clone https://github.com/fmtlib/fmt.git /tmp/fmt-src
+    cp -r /tmp/fmt-src/include/fmt $INSTALL_PREFIX/include/fmt
+    rm -rf /tmp/fmt-src
+    cat << EOF > $INSTALL_PREFIX/include/format
+#pragma once
+
+#define FMT_HEADER_ONLY
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <string>
+#include <string_view>
+#include <iterator>
+#include <utility>
+
+namespace std {
+    // std::format
+    template <typename... Args>
+    inline std::string format(fmt::format_string<Args...> fmt_str, Args&&... args) {
+        return fmt::format(fmt_str, std::forward<Args>(args)...);
+    }
+
+    // std::format_to
+    template <typename OutputIt, typename... Args>
+    inline OutputIt format_to(OutputIt out, fmt::format_string<Args...> fmt_str, Args&&... args) {
+        return fmt::format_to(out, fmt_str, std::forward<Args>(args)...);
+    }
+}
+EOF
+    g++ -std=c++20 format-check.cpp -o format-check
+    ./format-check
+cat << EOF > format-check-2.cpp
+#include <format>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    // std::format
+    std::string s1 = std::format("Hello {}!", "world");
+    std::cout << s1 << "\n";
+
+    // std::format_to
+    std::string buffer;
+    std::format_to(std::back_inserter(buffer), "Pi approx: {:.2f}", 3.14159);
+    std::cout << buffer << "\n";
+
+    return 0;
+}
+EOF
+    g++ -std=c++20 format-check-2.cpp -o format-check-2
+    ./format-check-2
+fi
+cd && rm -rf /tmp/format-check
+
 # DOLFINx
 git clone https://github.com/FEniCS/dolfinx.git /tmp/dolfinx-src
 cd /tmp/dolfinx-src
@@ -103,7 +166,6 @@ if [[ "$RELEASE_TYPE" == "release" ]]; then
     git checkout v0.9.0
 else
     git checkout main
-    patch -p 1 < $REPODIR/fenicsx/patches/02-drop-std-format-in-dolfinx-cpp
 fi
 sed -i "s|INSTALL_PREFIX_IN|${INSTALL_PREFIX}|g" $REPODIR/fenicsx/patches/01-pkg-config-path-in-dolfinx
 patch -p 1 < $REPODIR/fenicsx/patches/01-pkg-config-path-in-dolfinx
