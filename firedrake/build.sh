13f47ab832f71e676690c69f4326f0c6be4c510a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-10-04 08:47:17 +0200

    Drop firedrake patch 03-disable-Wincompatible-pointer-types-firedrake-cython

--- firedrake/build.sh
+++ firedrake/build.sh
@@ -81,7 +81,6 @@ if [[ "$RELEASE_TYPE" == "release" ]]; then
 else
     git checkout main
 fi
-patch -p 1 < $REPODIR/firedrake/patches/03-disable-Wincompatible-pointer-types-firedrake-cython
 patch -p 1 < $REPODIR/firedrake/patches/04-hardcode-omp-num-threads-in-firedrake
 PYTHONUSERBASE=$INSTALL_PREFIX python3 -m pip install --check-build-dependencies --no-build-isolation --user .
 cd && rm -rf /tmp/firedrake-src
