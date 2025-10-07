ff3e76bda7719b221536685cbe525bc216eb3086
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-10-07 08:23:21 +0200

    Bump firedrake version to 2025.10.1

--- firedrake/build.sh
+++ firedrake/build.sh
@@ -76,7 +76,7 @@ PYTHONUSERBASE=$INSTALL_PREFIX python3 -m pip install --user "setuptools>=77"
 git clone https://github.com/firedrakeproject/firedrake.git /tmp/firedrake-src
 cd /tmp/firedrake-src
 if [[ "$RELEASE_TYPE" == "release" ]]; then
-    git checkout 2025.10.0
+    git checkout 2025.10.1
     patch -p 1 < $REPODIR/firedrake/patches/05-unpin-petsc4py-slepc4py
 else
     git checkout main
