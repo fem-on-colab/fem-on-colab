71c6ea3fbbd4e5b188c3c0fe129e5b749a737bcd
Francesco Ballarin <francesco.ballarin@unicatt.it>
2026-02-07 10:42:29 +0100

    PETSc PR 8956 has changed the type of context variables. Patch dolfin with the new types, but only if the release_type is development (i.e., if dev version of PETSc is used). The patch will be extended to release_type==release at the next PETSc release

--- fenics/build.sh
+++ fenics/build.sh
@@ -77,6 +77,9 @@ patch -p 1 < $REPODIR/fenics/patches/10-c++-14-in-dolfin
 patch -p 1 < $REPODIR/fenics/patches/12-do-not-fiddle-with-dlopenflags-in-dolfin
 patch -p 1 < $REPODIR/fenics/patches/17-dolfin-bump-boost-minimum-version
 patch -p 1 < $REPODIR/fenics/patches/19-dolfin-jit-avoid-distutils
+if [[ "$RELEASE_TYPE" == "development" ]]; then
+    patch -p 1 < $REPODIR/fenics/patches/20-petsc-context-pointers-3.25-PR8956
+fi
 mkdir -p /tmp/dolfin-src/build
 cd /tmp/dolfin-src/build
 export UFC_DIR=$INSTALL_PREFIX
