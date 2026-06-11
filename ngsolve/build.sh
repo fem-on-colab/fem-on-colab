4ef97907cf963141cbcc1e4fcf98519095dacbd0
Francesco Ballarin <francesco.ballarin@unicatt.it>
2026-06-11 09:35:33 +0200

    Bump ngsxfem release to v2.1.2604

--- ngsolve/build.sh
+++ ngsolve/build.sh
@@ -96,7 +96,7 @@ cd && rm -rf /tmp/ngsolve-src
 git clone https://github.com/ngsxfem/ngsxfem.git /tmp/ngsxfem-src
 cd /tmp/ngsxfem-src
 if [[ "$RELEASE_TYPE" == "release" ]]; then
-    git checkout v2.1.2603
+    git checkout v2.1.2604
 else
     git checkout master
 fi
