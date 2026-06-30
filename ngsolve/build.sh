a4f2c39d4d3598e36c1bf0f8db62ff1eca72d6b6
Francesco Ballarin <francesco.ballarin@unicatt.it>
2026-06-30 12:11:27 +0200

    Bump netgen/ngsolve release tags to v6.2.2606, and ngsxfem to v2.1.2606

--- ngsolve/build.sh
+++ ngsolve/build.sh
@@ -32,7 +32,7 @@ NGSOLVE_ARCHIVE_PATH="skip" source ngsolve/install.sh
 git clone https://github.com/NGSolve/netgen.git /tmp/netgen-src
 cd /tmp/netgen-src
 if [[ "$RELEASE_TYPE" == "release" ]]; then
-    git checkout v6.2.2604
+    git checkout v6.2.2606
 else
     git checkout master
 fi
@@ -63,17 +63,13 @@ cd && rm -rf /tmp/netgen-src
 git clone https://github.com/NGSolve/ngsolve.git /tmp/ngsolve-src
 cd /tmp/ngsolve-src
 if [[ "$RELEASE_TYPE" == "release" ]]; then
-    git checkout v6.2.2604
+    git checkout v6.2.2606
 else
     git checkout master
 fi
 git submodule update --init
 patch -p 1 < $REPODIR/ngsolve/patches/01-petsc-external-libs
-if [[ "$RELEASE_TYPE" == "release" ]]; then
-    patch -p 1 < $REPODIR/ngsolve/patches/02-revert-load-mkl-pardiso-at-runtime.2604
-else
-    patch -p 1 < $REPODIR/ngsolve/patches/02-revert-load-mkl-pardiso-at-runtime
-fi
+patch -p 1 < $REPODIR/ngsolve/patches/02-revert-load-mkl-pardiso-at-runtime
 mkdir -p /tmp/ngsolve-src/build
 cd /tmp/ngsolve-src/build
 cmake \
@@ -96,7 +92,7 @@ cd && rm -rf /tmp/ngsolve-src
 git clone https://github.com/ngsxfem/ngsxfem.git /tmp/ngsxfem-src
 cd /tmp/ngsxfem-src
 if [[ "$RELEASE_TYPE" == "release" ]]; then
-    git checkout v2.1.2604
+    git checkout v2.1.2606
 else
     git checkout master
 fi
